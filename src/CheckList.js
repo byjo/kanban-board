a3ec56368fc184d52c6798a5193134cf21952e83
byjo <buyongjo@gmail.com>
2017-07-03 21:24:17 +0900

    chapter 2

--- src/CheckList.js
+++ src/CheckList.js
@@ -12,7 +12,16 @@ export class CheckList extends Component {
 
 		return (
 			<div className='checklist'>
-				<ul>{tasks}</ul>
+				<ul>
+					{this.props.tasks.map((task) => (
+								<li key={task.id} className='checklist__task'>
+									<input type='checkbox' defaultChecked={task.done} />
+									{task.name}
+									<a href='#' className='checklist__task--remove' />
+								</li>
+							))}
+				</ul>
+				<input type='text' className='checklist--add-task' placeholder='Type then hit Enter to add a task' />
 			</div>
 		);
 	}
--- src/List.js
+++ src/List.js
@@ -4,13 +4,14 @@ import Card from './Card.js'
 export class List extends Component {
 	render() {
 		let cards = this.props.cards.map((card) => {
-			return <Card key={card.id} 
-						id={card.id}
-						title={card.title} 
-						description={card.description} 
-						tasks={card.tasks}
-					 />
-		})
+							return <Card key={card.id} 
+										id={card.id}
+										title={card.title} 
+										description={card.description} 
+										color={card.color}
+										tasks={card.tasks}
+									 />
+						});
 		return (
 			<div className='list'>
 				<h1>{this.props.title}</h1>
